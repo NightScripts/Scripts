8d6b5db95bc66ecace9e7a68716252a555cccb2d
Night <166496112+NightScripts@users.noreply.github.com>
2024-11-24 15:55:28 +0800

    Update Rayfield Window.lua
    My blind ass forgot to change smth

--- Nocturne/Rayfield Window.lua
+++ Nocturne/Rayfield Window.lua
@@ -1,32 +1,34 @@
-local Window = Rayfield:CreateWindow({
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+return Rayfield:CreateWindow({
    Name = "Nocturne | Open Source | ID:" .. tostring(game.PlaceId),
-   Icon = 0, 
+   Icon = 0,
    LoadingTitle = "Kamehameha Simulator",
    LoadingSubtitle = "by NightScripts",
    Theme = "Default",
    DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+   DisableBuildWarnings = false,
 
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = Nocturne, 
-      FileName = "Nocturne"
+      FolderName = Nocturne,
+      FileName = "Nocturne",
    },
 
    Discord = {
-      Enabled = false, 
-      Invite = "noinvitelink", 
-      RememberJoins = true 
-    },
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true,
+   },
 
    KeySystem = false,
    KeySettings = {
       Title = "Untitled",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "Key", 
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key",
       SaveKey = true,
       GrabKeyFromSite = false,
-      Key = {"Hello"} 
-    }
+      Key = {"Hello"},
+   },
 })
